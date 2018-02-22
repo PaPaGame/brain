4801d4192e25c8f0699089ac2c1c14bc1b7413ac
振亮 孙 <solszl@163.com>
2018-02-22 23:33:49 +0800

    - remove ugly code

--- server/app/routes/index.js
+++
@@ -1,7 +0,0 @@
-import Router from "koa-router";
-
-const router = Router();
-const loginRouter = require("../routes/user");
-router.use("/api", loginRouter.routes(), loginRouter.allowedMethods());
-
-export default router;
--- server/app/routes/user.js
+++
@@ -1,9 +0,0 @@
-const Router = require("koa-router");
-const UserController = require("../../controller/userController.js");
-
-const childRouter = new Router();
-
-childRouter.post("/login", UserController.Login);
-childRouter.post("/regist", UserController.Regist)
-
-module.exports = childRouter;
