efdf8b384e14115fddeb3ddbb43a5d127a01f8f3
zhenliang.sun <zhenliang.sun@vhall.com>
2018-02-26 15:18:55 +0800

    * 修改table  提出组件

---
+++ src/components/table.vue
@@ -0,0 +1,64 @@
+<template>
+    <section>
+        <el-table ref="eduTable"
+            :data="tableData"
+            @cell-click="cellClick"
+            style="with:100%"
+            class="edu-fix"
+            border>
+            <el-table-column v-for="(value, index) in tableColumns"
+                :key="index"
+                :prop="value.prop"
+                :label="value.label"
+                :width="value.width ? value.width :''"
+                :className="value.className">
+                <template slot-scope="scope">
+                    <div v-if="value.slotName">
+                        <slot :name="value.slotName"
+                            :row="scope.row" />
+                    </div>
+                    <div v-else
+                        v-html="value.template ? value.template(scope.row) : scope.row[value.prop]"></div>
+                </template>
+            </el-table-column>
+        </el-table>
+        <section class="page-container"
+            v-if="showPage && totalCount>pageSize">
+            <el-pagination layout="prev, pager, next"
+                :page-size="pageSize"
+                :total="totalCount"
+                :current-page.sync="indexPage"
+                @current-change="handleCurrentPageChange"></el-pagination>
+        </section>
+    </section>
+</template>
+
+<script>
+export default {
+    props: {
+        cellClick: { type: Function, default: function () { } },
+        tableColumns: { type: Array, default: [] },
+        tableData: Array,
+        showPage: { type: Boolean, default: true },
+        totalCount: Number,
+        pageSize: { type: Number, default: 10 }
+    },
+    data() {
+        return {
+            indexPage: 1
+        }
+    },
+    methods: {
+        initPage() {
+            this.indexPage = 1;
+        },
+        handleCurrentPageChange(val) {
+            this.$emit("pageChange", val);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
--- src/views/student/index.vue
+++ src/views/student/index.vue
@@ -4,32 +4,57 @@
         <!-- 过滤器 -->
         <div>
             <div class="filter-container">
-                <el-input ref="tiSearch" type="input" :placeholder="$t('student.searchName')" class="filter-item" style="width:200px" clearable></el-input>
-                <el-button type="primary" icon="el-icon-search" class="filter-item" @click="btnSearchClickHandler">{{$t('student.search')}}</el-button>
-                <el-button type="primary" icon="el-icon-edit" class="filter-item" style="margin-left: 10px;" @click="operateHandler(null,'create')">{{$t('student.add')}}</el-button>
+                <el-input ref="tiSearch"
+                    type="input"
+                    :placeholder="$t('student.searchName')"
+                    class="filter-item"
+                    style="width:200px"
+                    clearable></el-input>
+                <el-button type="primary"
+                    icon="el-icon-search"
+                    class="filter-item"
+                    @click="btnSearchClickHandler">{{$t('student.search')}}</el-button>
+                <el-button type="primary"
+                    icon="el-icon-edit"
+                    class="filter-item"
+                    style="margin-left: 10px;"
+                    @click="operateHandler(null,'create')">{{$t('student.add')}}</el-button>
             </div>
         </div>
         <div class="leftContainer">
             <class-list></class-list>
         </div>
         <div class="rightContainer">
-            <el-table :data="studentList" border style="width:100%" fit>
-                <el-table-column align="center" :label="$t('student.secondName')" width="90" prop="master">
+            <el-table :data="studentList"
+                border
+                style="width:100%"
+                fit>
+                <el-table-column align="center"
+                    :label="$t('student.secondName')"
+                    width="90"
+                    prop="master">
                     <template slot-scope="scope">
                         <span>{{scope.row.secondName}}</span>
                     </template>
                 </el-table-column>
-                <el-table-column align="center" :label="$t('student.firstName')" width="60">
+                <el-table-column align="center"
+                    :label="$t('student.firstName')"
+                    width="60">
                     <template slot-scope="scope">
                         <span>{{scope.row.firstName}}</span>
                     </template>
                 </el-table-column>
-                <el-table-column align="center" :label="$t('student.school')" width="150">
+                <el-table-column align="center"
+                    :label="$t('student.school')"
+                    width="150">
                     <template slot-scope="scope">
                         <span>{{scope.row.school}}</span>
                     </template>
                 </el-table-column>
-                <el-table-column align="center" :label="$t('student.group')" width="270" prop="name">
+                <el-table-column align="center"
+                    :label="$t('student.group')"
+                    width="270"
+                    prop="name">
                     <template slot-scope="scope">
                         <span>{{scope.row.group}}</span>
                     </template>
@@ -37,23 +62,51 @@
             </el-table>
             <!-- 翻页 -->
             <div class="pagination-container">
-                <el-pagination background @size-change="pageSizeChangeHandler" @current-change="pageCurrentChangeHandler" :current-page="1" :page-sizes="[5,10,20]" :page-size="10" :total="2" layout="total, sizes, prev, pager, next, jumper">
+                <el-pagination background
+                    @size-change="pageSizeChangeHandler"
+                    @current-change="pageCurrentChangeHandler"
+                    :current-page="1"
+                    :page-sizes="[5,10,20]"
+                    :page-size="10"
+                    :total="2"
+                    layout="total, sizes, prev, pager, next, jumper">
                 </el-pagination>
             </div>
         </div>
+        <edu-table :tableColumns="tableColumns"
+            :tableData="studentList"
+            :totalCount="totalCount"
+            :pageSize="studentModel.pagesize"
+            @pageChange="pageChange"
+            ref="table">
+        </edu-table>
     </div>
 </template>
 
 <script>
 import ClassList from "@/components/ClassList";
 import { FetchStudents } from "@/api/student";
+import table from "@/components/table";
 export default {
     components: {
-        ClassList
+        ClassList,
+        "edu-table": table
     },
     data() {
         return {
-            studentList: []
+            studentList: [],
+            studentModel: {
+                subject: "",
+                curr_page: 1,
+                pagesize: 10,
+                type: "page"
+            },
+            tableColumns: [
+                { prop: "secondName", label: this.$t('student.secondName'), width: '90' },
+                { prop: "firstName", label: this.$t('student.firstName'), width: '90' },
+                { prop: "school", label: this.$t('student.school'), width: '90' },
+                { prop: "group", label: this.$t('student.group'), width: '90' },
+            ]
         };
     },
     methods: {
@@ -63,6 +116,7 @@ export default {
     beforeMount() {
         FetchStudents().then(res => {
             this.studentList = res.students;
+            this.studentList = this.studentList.concat(this.studentList).concat(this.studentList).concat(this.studentList).concat(this.studentList).concat(this.studentList)
         })
     }
 }
