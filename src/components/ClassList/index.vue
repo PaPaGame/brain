e006de76d76b12eec8b7174fecaead4f4c3124c6
振亮 孙 <solszl@163.com>
2018-02-28 00:31:12 +0800

    * 搜索建议

--- src/components/ClassList/index.vue
+++ src/components/ClassList/index.vue
@@ -1,10 +1,19 @@
 <template>
     <div class="classList">
-        <el-autocomplete :placeholder="$t('student.inputGroupName')"
+        <el-autocomplete popper-class="my-autocomplete"
+            v-model="state3"
+            :fetch-suggestions="querySearch"
+            :placeholder="$t('student.inputGroupName')"
             :trigger-on-focus="false"
-            @select="selectHandler"
-            class="inline-input"
-            :fetch-suggestions="querySearch"></el-autocomplete>
+            @select="handleSelect">
+            <i class="el-icon-edit el-input__icon"
+                slot="suffix"
+                @click="handleIconClick">
+            </i>
+            <template slot-scope="props">
+                <div class="name">{{ props.item.name }}({{props.item.student.length}})</div>
+            </template>
+        </el-autocomplete>
         <span>{{$t('student.inputTips')}}</span>
         <ul class="classBox">
             <template v-for="group in groups">
@@ -26,24 +35,40 @@ export default {
     name: 'classList',
     data() {
         return {
+            state3: '',
+            state2: ""
         }
     },
     methods: {
-        selectHandler(item) {
+        handleSelect(item) {
             console.log(item);
         },
         groupItemClickHandler(itemInfo) {
             this.$emit("groupListClick", itemInfo);
         },
         querySearch(queryString, cb) {
-            let result = [];
-            result = queryString ? this.groups.filter(this.createFilter(queryString)) : result;
+            var groups = this.groups;
+            var result = queryString ? groups.filter(this.createFilter(queryString)) : groups;
+
+            console.log(result);
             cb(result);
+
+            // var groups = this.groups;
+            // var results = queryString ? groups.filter(this.createFilter(queryString)) : groups;
+            // cb(results);
         },
         createFilter(queryString) {
             return group => {
-                return (group.name.toLowerCase().indexOf(queryString.toLowerCase()) === 0)
+                let result = group.name.toLowerCase().indexOf(queryString.toLowerCase()) === 0;
+                // console.log(result);
+                return result
             }
+        },
+        handleSelect(item) {
+            console.log(item);
+        },
+        handleIconClick(ev) {
+            console.log(ev);
         },
         ...mapActions(["getGroupList"])
     },
@@ -74,4 +99,23 @@ export default {
 
 <style lang="scss" scoped>
 @import "./index.scss";
+.my-autocomplete {
+  li {
+    line-height: normal;
+    padding: 7px;
+
+    .name {
+      text-overflow: ellipsis;
+      overflow: hidden;
+    }
+    .addr {
+      font-size: 12px;
+      color: #b4b4b4;
+    }
+
+    .highlighted .addr {
+      color: #ddd;
+    }
+  }
+}
 </style>
