6cd2102053d3c268fb83a52c15e4ab8ea7bb5785
振亮 孙 <solszl@163.com>
2018-03-05 22:22:19 +0800

    完善添加学生

--- server/src/dao/DaoStudent.js
+++ server/src/dao/DaoStudent.js
@@ -1,5 +1,6 @@
 var DaoBase = require("./DaoBase");
 var StudentModel = require("../models").student;
+var UserModel = require("../models").user;
 var util = require("util");
 
 var student;
@@ -15,8 +16,30 @@ StudentDao.prototype.addStudent = async (userinfo, callback) => {
         return callback({ err: 'err parameter' });
 
     let student = new StudentModel({
-
+        username: userinfo.username,
+        group: userinfo.group,
+        school: userinfo.school,
+        articleLevel: userinfo.article || [],
+        validateTime: 10
     });
+
+    let result = await StudentModel.create(student).then(res => {
+        let uid = res._id;
+        let user = new UserModel({
+            username: userinfo.username,
+            password: userinfo.password,
+            status: userinfo.status,
+            role: "1",
+            phone: userinfo.phone,
+            mail: userinfo.mail,
+            lastLoginTime: new Date().toISOString(),
+            lastLoginIP: "",
+            school: userinfo.school,
+            uid: uid
+        });
+
+        return UserModel.create(user);
+    })
 }
 
 module.exports = StudentDao;
