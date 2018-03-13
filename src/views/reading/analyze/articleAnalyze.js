a0847bcb0a5ba65c11c296cc6dde55b31fa534ac
振亮 孙 <solszl@163.com>
2018-03-14 00:32:20 +0800

    解析

--- src/views/reading/analyze/articleAnalyze.js
+++ src/views/reading/analyze/articleAnalyze.js
@@ -6,7 +6,22 @@ function ArticleAnalyze() {
 ArticleAnalyze.prototype.start = (originData) => {
     // 传进来的就是obj
     // console.log(originData);
+    let result = {};
+    // 解析标题
+    result.title = originData.full_title;
 
+    // 解析 文章内容
+    let pages = originData.pages;
+    pages.forEach(ele => {
+        result.pages.push(ele.id);
+    })
+
+    // 解析灯泡
+    result.tais = [];
+    let tais = originData.tai;
+    tais.forEach(ele => {
+        result.tais.push(ele.id);
+    });
 }
 
 module.exports = ArticleAnalyze;
