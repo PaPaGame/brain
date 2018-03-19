b84c377bdd68ade1f2ed782959e2181b1812145c
振亮 孙 <solszl@163.com>
2018-03-20 02:44:58 +0800

    * 让灯泡能播放声音

--- src/views/reading/dialog/tai.vue
+++ src/views/reading/dialog/tai.vue
@@ -2,14 +2,21 @@
     <edu-dialog :isShow="dialogVisible"
         @close="close"
         :title="this.question.question">
-        <el-radio-group v-model="radioModel">
+        <el-radio-group v-model="radioLabel"
+            @change="onRadioChange(radioLabel)">
             <el-radio v-for="(option,index) in question.answers"
-                :key="option.answer"
+                :key="index"
                 :label="option.answer"></el-radio>
         </el-radio-group>
-        <el-button>
+        <el-button @click="submitAnswer">
             <i class="iconfont icon-laba"></i>{{$t('reading.commit')}}</el-button>
-        <audio ref="taiAudio"></audio>
+        <transition name="list">
+            <div v-show="hits!=''">{{hits}}</div>
+        </transition>
+
+        <audio ref="taiAudio"
+            autoplay
+            @ended="playend"></audio>
     </edu-dialog>
 </template>
 
@@ -23,7 +30,9 @@ export default {
     data() {
         return {
             dialogVisible: false,
-            radioModel: ""
+            radioLabel: "",
+            findAnswer: null,
+            hits: ""
         };
     },
     props: {
@@ -39,9 +48,50 @@ export default {
         }
     },
     methods: {
+        onRadioChange(lbl) {
+            let ans = this.question.answers;
+            for (let i = 0; i < ans.length; i++) {
+                let answer = ans[i];
+                if (answer.answer == lbl) {
+                    this.findAnswer = answer;
+                    break;
+                }
+            }
+
+            if (!this.findAnswer)
+                return;
+
+            this.playSound(this.findAnswer.audio);
+        },
+        submitAnswer() {
+            if (!this.findAnswer) {
+                return;
+            }
+
+            let hits = this.findAnswer.hints;
+            let rndIdx = Math.floor(Math.random() * 2);
+            let hit = hits[rndIdx];
+            this.hits = hit.text;
+            this.playSound(hit.audio);
+            // TODO: 发送数据
+        },
         close() {
             this.dialogVisible = false;
+            this.hits = "";
             this.$emit("close");
+        },
+        playSound(id) {
+            this.$refs.taiAudio.src = `http://192.168.199.136:9050/dist/${this.dirName}/audio/${id}`;
+        },
+        playend() {
+            if (!this.findAnswer)
+                return;
+
+            if (this.findAnswer.correct == true) {
+                setTimeout(() => {
+                    this.close();
+                }, 5000);
+            }
         },
         ...mapActions(["getTaiInfo"])
     },
