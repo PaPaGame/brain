5d23506ef030050459d0f6870effc07a68b99b1c
振亮 孙 <solszl@163.com>
2018-04-22 23:42:46 +0800

    这是一个提交测试，测试个人仓库到组织仓库

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,7 @@
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
       <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
+      <span>这是一个提交测试</span>
     </ul>
   </div>
 </template>
