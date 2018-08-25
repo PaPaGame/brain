53e0a8f258a26e41cc657230b63dd792f27f1139
zhenliang.sun <solszl@163.com>
2018-08-25 16:32:56 +0800

    * 修改文章等级序号  sql 添加

--- build/build.js
+++ build/build.js
@@ -1,7 +1,6 @@
 'use strict'
 require('./check-versions')()
 
-process.env.NODE_ENV = 'production'
 const ora = require('ora')
 const rm = require('rimraf')
 const path = require('path')
@@ -10,7 +9,7 @@ const webpack = require('webpack')
 const config = require('../config')
 const webpackConfig = require('./webpack.prod.conf')
 
-const spinner = ora('building for production...')
+const spinner = ora('building for ' + process.env.BUILD_ENV + '...')
 spinner.start()
 
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ Vue.use(ElementUI, {
     i18n: (key, value) => i18n.t(key, value)
 });
 
-Vue.config.productionTip = false;
 /* eslint-disable no-new */
 new Vue({
     el: '#app',
